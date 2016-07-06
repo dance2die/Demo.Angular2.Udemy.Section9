d51f0c30f0546c63df563a4b74269dabd15a7704
dance2die <dontbothermewithspam@gmail.com>
2016-07-06 06:42:00 -0400

    Added a post method using Http module

--- app/post.service.ts
+++ app/post.service.ts
@@ -2,11 +2,18 @@ import {Http} from 'angular2/http';
 import 'rxjs/add/operator/map';
 
 export class PostService {
+    private _url = "http://jsonplaceholder.typicode.com/posts";
+
     constructor(private _http: Http) {
     }
     
     getPost() {
-        return this._http.get("http://jsonplaceholder.typicode.com/posts")
+        return this._http.get(this._url)
+            .map(response => response.json());
+    }
+
+    createPost(post) {
+        return this._http.post(this._url, JSON.stringify(post))
             .map(response => response.json());
     }
 }
